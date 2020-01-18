0b4bd7da9ecdfc2b86b941b4c442d7e146d7a5ad
AJ <ascott1083@yahoo.com>
2020-01-18 01:04:56 -0600

    added client folder

---
+++ client/build/precache-manifest.e5d3abcbf8f754f5df117ad8e1f8b789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "210c4a669ab86022908e",
+    "url": "/static/css/main.b39f2307.chunk.css"
+  },
+  {
+    "revision": "210c4a669ab86022908e",
+    "url": "/static/js/main.3cd5c9f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dee874e4218542f47b6b",
+    "url": "/static/js/2.bd9c1e72.chunk.js"
+  },
+  {
+    "revision": "961c5f43fad9a66585a9950bc77bfbd3",
+    "url": "/static/media/bg1.961c5f43.jpg"
+  },
+  {
+    "revision": "88fb303bc8afd413d70e4b6e590c08a3",
+    "url": "/index.html"
+  }
+];
